4de7ecba9531c797e70a9f4ecd830733cb1f8016
Thierry Moisan <thierry.moisan@gmail.com>
2026-08-29 16:02:40 -0400

    pandoc-crossref: revision bump for pandoc 3.11

--- Formula/p/pandoc-crossref.rb
+++ Formula/p/pandoc-crossref.rb
@@ -4,7 +4,7 @@ class PandocCrossref < Formula
   url "https://github.com/lierdakil/pandoc-crossref/archive/refs/tags/v0.3.25.tar.gz"
   sha256 "cb42b8319a59f258fea191e4660b62bdd9a90a9099322ae0f17203bc5986498a"
   license "GPL-2.0-or-later"
-  revision 1
+  revision 2
 
   bottle do
     sha256 cellar: :any, arm64_tahoe:   "866aa0a29786db950b528e19151f6638bce273a5cd40c1df80c477feda0b1932"
